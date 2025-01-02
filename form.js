27ab1d57fc3aef49dbe0954b4dcb023cb6292834
aminalundin <lundinamina1@gmail.com>
2025-01-02 22:31:19 +0100

    created DOMs on form.js

---
+++ form.js
@@ -0,0 +1,6 @@
+// DOMs
+const form = document.querySelector("form");
+const textInput = document.querySelector("input#text");
+const aside = document.querySelector("aside");
+const ul = document.querySelector("ul");
+const nameInput = document.querySelector("input#name");
