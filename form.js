23832c928f1810c8f53ef976d9be0d0753709940
aminalundin <lundinamina1@gmail.com>
2025-01-02 22:35:21 +0100

    created eventlistener for form on form.js

--- form.js
+++ form.js
@@ -4,3 +4,22 @@ const textInput = document.querySelector("input#text");
 const aside = document.querySelector("aside");
 const ul = document.querySelector("ul");
 const nameInput = document.querySelector("input#name");
+
+// Event listeners
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+  
+    const inputValueName = nameInput.value;
+    let inputValue = textInput.value;
+   
+    aside.appendChild(ul);
+
+    let li = document.createElement("li");
+    ul.appendChild(li);
+
+    li.innerHTML = `<button class="delete">&#10005;</button><button class="save">&hearts;</button><a href="${inputValue}" target="_blank">${inputValueName}</a></li>`;
+
+    textInput.value = "";
+    nameInput.value = "";
+});
+
